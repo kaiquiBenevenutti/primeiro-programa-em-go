2706ba48bc277e4ec362af2e7e0bace954f8beb9
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2024-09-17 11:37:26 -0300

    Arrumando erro de verificação

--- main.go
+++ main.go
@@ -8,41 +8,50 @@ import (
 )
 
 func main() {
+	for {
 
-	var resposta string
+		var resposta string
 
-	fmt.Print("Digite o seu nome: ")
-	_, err := fmt.Scanln(&resposta)
-	if err != nil {
-		fmt.Println("Erro ao ler a entrada:", err)
+		fmt.Print("Digite o seu nome: ")
+		_, err := fmt.Scanln(&resposta)
+		if err != nil {
+			fmt.Println("Erro ao ler a entrada:", err)
+			waitForUserInput()
+			continue
+		}
+
+		fmt.Println("Ola", resposta, "como voce esta?")
+
+		var resposta2 string
+		resposta2 = strings.ToLower(resposta2)
+
+		for {
+			_, err2 := fmt.Scanln(&resposta2)
+
+			if err2 != nil {
+				fmt.Println("Erro ao ler a entrada:", err2)
+				waitForUserInput()
+				break
+			}
+
+			if resposta2 == "bem" {
+				fmt.Println("Que otimo!")
+				break
+			} else if resposta2 == "mau" {
+				fmt.Println("Ah, que pena.")
+				break
+			} else {
+				fmt.Println("Não entendi sua resposta, responda com 'bem' ou 'mau'.")
+				fmt.Println(resposta, "como voce esta?")
+			}
+		}
+
+		fmt.Println("Pressione Enter para sair...")
+		waitForUserInput()
 		return
 	}
+}
 
-	fmt.Println("Ola", resposta, "como voce esta?")
-
-	var resposta2 string
-	resposta2 = strings.ToLower(resposta2)
-
-	for {
-		_, err2 := fmt.Scanln(&resposta2)
-
-		if err2 != nil {
-			fmt.Println("Erro ao ler a entrada:", err2)
-			return
-		}
-
-		if resposta2 == "bem" {
-			fmt.Println("Que otimo!")
-			break
-		} else if resposta2 == "mau" {
-			fmt.Println("Ah, que pena.")
-			break
-		} else {
-			fmt.Println("Não entendi sua resposta, responda com 'bem' ou 'mau'.")
-			fmt.Println(resposta, "como voce esta?")
-		}
-	}
-
-	fmt.Println("Pressione Enter para sair...")
+func waitForUserInput() {
 	bufio.NewReader(os.Stdin).ReadBytes('\n')
 }
